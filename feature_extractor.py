22f6b21ac95557a7c8106f3dc28a0a76186161a3
emonkarmoker <emonkarmaker101@gmail.com>
2025-10-22 00:17:07 +0600

    break

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,4 +1,24 @@
-import os
-actors = os.listdir(r'E:\ML Practice\Starting over\bollywood_face\Bollywood_celeb_face_localized\data')
+'''import os
+#import pickle
+#actors = os.listdir(r'E:\ML Practice\Starting over\bollywood_face\Bollywood_celeb_face_localized\data')
 
-print(actors)
+filenames=[]
+for actor in actors:
+    for file in os.listdir(os.path.join(r'E:\ML Practice\Starting over\bollywood_face\Bollywood_celeb_face_localized\data',actor)):
+        filenames.append(os.path.join(r'E:\ML Practice\Starting over\bollywood_face\Bollywood_celeb_face_localized\data',actor,file))
+pickle.dump(filenames, open('filenames.pkl','wb'))'''
+
+
+from tensorflow.keras.preprocessing import image
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+import numpy as np
+import pickle
+filenames=pickle.load(open('filenames.pkl','rb'))
+model=VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
+def feature_extractor(img_path,model):
+    img=image.load_img(img_path,target_size=(224,224))
+    img_array=image.img_to_array(img)
+    expanded_img=np.expand_dims(img_array,axis=0)
+    preprocessed_img=preprocess_input(expanded_img)
+    model.predict(preprocessed_img).flatten()
