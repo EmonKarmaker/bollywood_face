3c972493baa28c8a7ba426dcb2c319aaf01aa360
emonkarmoker <emonkarmaker101@gmail.com>
2025-10-23 12:50:25 +0600

    feature extraction complete

--- feature_extractor.py
+++ feature_extractor.py
@@ -14,6 +14,7 @@ from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
 import numpy as np
 import pickle
+from tqdm import tqdm
 filenames=pickle.load(open('filenames.pkl','rb'))
 model=VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
 def feature_extractor(img_path,model):
@@ -22,3 +23,11 @@ def feature_extractor(img_path,model):
     expanded_img=np.expand_dims(img_array,axis=0)
     preprocessed_img=preprocess_input(expanded_img)
     model.predict(preprocessed_img).flatten()
+    result=model.predict(preprocessed_img).flatten()
+    return result
+
+features=[]
+for file in tqdm(filenames):
+    features.append(feature_extractor(file,model))
+
+pickle.dump(features,open('embedding.pkl','wb'))
